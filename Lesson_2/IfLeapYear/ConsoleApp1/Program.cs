30e96146d53dbe4a3e0176f79ee50dc7c41c0060
genakvitkov <hdghg88@gmail.com>
2024-02-03 15:21:38 +0700

    IfLeapYear 1.0.0

---
+++ Lesson_2/IfLeapYear/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Давайте определим, является ли год високосным");
+            Console.WriteLine("Введите номер года!");
+            string userString = Console.ReadLine();
+
+            while (!uint.TryParse(userString, out uint result)) 
+                { 
+                Console.WriteLine("Не похоже на номер года...");
+                Console.WriteLine("Введите номер года!");
+                userString = Console.ReadLine();
+                }
+
+            uint yearNumber = uint.Parse(userString);
+
+            if (yearNumber % 4 == 0) //&& (yearNumber % 400 == 0) && (yearNumber % 100 != 0))
+            {
+                Console.WriteLine($"Год {yearNumber} является високосным");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine($"Год {yearNumber} не является високосным");
+                Console.ReadLine();
+            }
+        }
+    }
+}
