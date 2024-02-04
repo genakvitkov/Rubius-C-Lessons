09d9235f7014706e0055bb2bad4a116516e5c6b2
genakvitkov <hdghg88@gmail.com>
2024-02-05 00:37:34 +0700

    1.1.0

--- Lesson_2/IfLeapYear/ConsoleApp1/Program.cs
+++ Lesson_2/IfLeapYear/ConsoleApp1/Program.cs
@@ -8,31 +8,47 @@ namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
+        public static string InputAndValidate(string userString)
         {
-            Console.WriteLine("Давайте определим, является ли год високосным");
-            Console.WriteLine("Введите номер года!");
-            string userString = Console.ReadLine();
-
-            while (!uint.TryParse(userString, out uint result)) 
-                { 
+            while (!uint.TryParse(userString, out uint garbage))
+            {
                 Console.WriteLine("Не похоже на номер года...");
                 Console.WriteLine("Введите номер года!");
                 userString = Console.ReadLine();
-                }
 
-            uint yearNumber = uint.Parse(userString);
+            }
+
+            return userString;
+        }
+
+        public static void IfLeapYear()
+        {
+            uint yearNumber = uint.Parse(InputAndValidate(Console.ReadLine()));
 
             if ((yearNumber % 4 == 0 && yearNumber % 100 != 0) || yearNumber % 400 == 0)
             {
                 Console.WriteLine($"{yearNumber} год является високосным");
-                Console.ReadLine();
             }
             else
             {
                 Console.WriteLine($"{yearNumber} год не является високосным");
-                Console.ReadLine();
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Давайте определим, является ли год високосным!");
+            Console.WriteLine("Введите номер года");
+            while (true)
+            {
+                IfLeapYear();
             }
         }
     }
 }
+
+/* Я конечно немного перемудрил с программой, сделал много того, что не требовалось по заданию, и не уверен, что правильно это сделал,
+ * но хотелось поиграться с правилами нового для меня языка, плюс я тестировал программу на своих друзьях и все сказали, что "чё за прикол, 
+ * один раз можно год ввести, а с повторным вводом программа вылетает", поэтому я учёл пользовательские пожелания))) в следующий раз постараюсь
+ * к заданиям более буквально подходить */
