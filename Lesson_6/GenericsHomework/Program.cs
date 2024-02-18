1f8d501867779d3386ceac28a6afac05961ea80c
genakvitkov <hdghg88@gmail.com>
2024-02-19 01:12:22 +0700

    Домашнее задание 6

---
+++ Lesson_6/GenericsHomework/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsHomework
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Stack<int> Integers = new Stack<int>();
+            Stack<double> Doubles = new Stack<double>();
+            Stack<string> Strings = new Stack<string>();
+
+            Integers.Push(1);
+            Integers.Push(2);
+            Integers.Push(3);
+
+            if (Integers.IsEmpty()) 
+            {
+                throw new Exception("Стэк пуст");
+            }
+
+            Integers.Pop();
+            Integers.Peek();
+            Integers.Clear();
+
+            if (Integers.IsEmpty()) 
+            {
+                Console.WriteLine("Всё получилось");
+            }
+            else
+            {
+                Console.WriteLine("Что-то пошло не так");
+            }
+
+            Doubles.Push(11.3);
+            Doubles.Push(2.57);
+            Doubles.Push(3.8);
+
+            if (Doubles.IsEmpty())
+            {
+                throw new Exception("Стэк пуст");
+            }
+
+            Doubles.Pop();
+            Doubles.Peek();
+            Doubles.Clear();
+
+            if (Doubles.IsEmpty())
+            {
+                Console.WriteLine("Всё получилось");
+            }
+            else
+            {
+                Console.WriteLine("Что-то пошло не так");
+            }
+
+            Strings.Push("Наследование");
+            Strings.Push("Инкапсуляция");
+            Strings.Push("Полиморфизм");
+
+            if (Strings.IsEmpty())
+            {
+                throw new Exception("Стэк пуст");
+            }
+
+            Strings.Pop();
+            Strings.Peek();
+            Strings.Clear();
+
+            if (Strings.IsEmpty())
+            {
+                Console.WriteLine("Всё получилось");
+            }
+            else
+            {
+                Console.WriteLine("Что-то пошло не так");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
